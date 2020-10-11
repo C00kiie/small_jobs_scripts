98174763387ba9e3f3815d05f89df7599eb3a97a
SomeWeirdName <30579674+C00kiie@users.noreply.github.com>
2020-10-11 06:53:47 -0400

    Create count_occurences.sh

---
+++ count_occurences.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Author: tyr4n7
+# This is only compatiable with bash 4.0 or higher since it uses an associative hashmap
+# can be used to count entity of a certain kind, it cuts the first field and counts it
+# for example to count the number of requests an IP did on apache server
+# we can just `cat apache_log | bash count_occurences.sh` 
+
+
+declare -A hashmap
+
+for i in $(cat $i | cut -d' ' -f1);
+do
+	let hashmap[$i]++;
+done
+
+counter=0;
+echo entity: count
+for j in "${!hashmap[@]}";
+do
+	echo "$j:${hashmap[$j]}"
+done
